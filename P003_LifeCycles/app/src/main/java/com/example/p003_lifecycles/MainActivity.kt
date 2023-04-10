0fd1cc98c0a2b4cd05cc8d0111444d6c17038599
DenisLuba <delubis@mail.ru>
2023-04-11 01:48:32 +0300

    Finished P003_LifeCycles

--- P003_LifeCycles/app/src/main/java/com/example/p003_lifecycles/MainActivity.kt
+++ P003_LifeCycles/app/src/main/java/com/example/p003_lifecycles/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.p003_lifecycles
 
 import android.content.Intent
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import com.example.p003_lifecycles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
-    companion object {
-        fun log(message: String) = Log.d("myLogs", message)
-    }
+    fun log(message: String) = Log.d(TAG, message)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +33,39 @@ class MainActivity : AppCompatActivity() {
         binding.buttonMinimizeApp.setOnClickListener {
             moveTaskToBack(false)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        log("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        log("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        log("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        log("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        log("onSaveInstanceState")
+    }
+
+    companion object {
+        @JvmStatic val TAG = "myLogs"
     }
 }
--- P003_LifeCycles/app/src/main/java/com/example/p003_lifecycles/SecondaryLifecycleActivity.kt
+++ P003_LifeCycles/app/src/main/java/com/example/p003_lifecycles/SecondaryLifecycleActivity.kt
@@ -2,10 +2,49 @@ package com.example.p003_lifecycles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SecondaryLifecycleActivity : AppCompatActivity() {
+
+    private fun log(message: String) = Log.d(TAG, message)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        log("onCreate, savedState = $savedInstanceState")
         setContentView(R.layout.activity_secondary_lifecycle)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        log("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        log("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        log("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        log("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        log("onSaveInstanceState")
+    }
+
+    companion object {
+        @JvmStatic val TAG = "myLogs"
     }
 }
